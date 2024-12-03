a4e948d0dfbb0bca8116fce3321e332675bdda6d
jams.sanchez <james.sanchez@laplateforme.io>
2024-12-03 21:14:24 +0100

    changement de la langue vers francais | ajout : un pré-lancement du jeu pour afficher un titre et permettant ainsi un retour au jeu si celui ci termine à égalité + un systeme de verification pour valider une victoire ou une égalité | corrections : erreurs lié aux mauvaises informations données par les joueurs + changement entier de la fonction changement de joueurs || le jeu fonctionne

--- Tik_Tak_James/tik_tak_james.py
+++ Tik_Tak_James/tik_tak_james.py
@@ -1,104 +1,166 @@
-# info
 tour = 1
 
-# players
-players = [1, 2]
-user = "X"
-bot = "O"
-symbol = ""
+# joueurs
+joueur_1= "X"
+joueur_2= "O"
+symbole = ""
 
 # tableau
 board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-line1 = board[0]
-line2 = board[1]
-line3 = board[2]
-            
-# switch players between rounds
-def switch_player():
+ligne1 = board[0]
+ligne2 = board[1]
+ligne3 = board[2]
+
+# Vérifie si un joueur a une ligne gagnante OU si la partie est égalité
+def verifie():
+    # Joueur 1 - verification horizontale
+    if board[0] == ["X", "X", "X"] \
+    or board[1] == ["X", "X", "X"] \
+    or board[2] == ["X", "X", "X"]:
+        print("HOORAY!")
+        print("Joueur 1 wins")
+        return True
+    # Joueur 1 - verification verticale
+    elif board[0][0] == joueur_1 and board[1][0] == joueur_1 and board[2][0] == joueur_1 \
+    or board[0][1] == joueur_1 and board[1][1] == joueur_1 and board[2][1] == joueur_1 \
+    or board[0][2] == joueur_1 and board[1][2] == joueur_1 and board[2][2] == joueur_1:
+        print("HOORAY!")
+        print("Joueur 1 wins")
+        return True
+    # Joueur 1 - vérification diagonale
+    elif board[0][0] == joueur_1 and board[1][1] == joueur_1 and board[2][2] == joueur_1 \
+    or board[0][2] == joueur_1 and board[1][1] == joueur_1 and board[2][0] == joueur_1:
+        print("HOORAY!")
+        print("Joueur 1 wins")
+        return True
+    # Joueur 2 - verification horizontale
+    if board[0] == ["O", "O", "O"] \
+    or board[1] == ["O", "O", "O"] \
+    or board[2] == ["O", "O", "O"]:
+        print("HOORAY!")
+        print("Joueur 2 wins")
+        return True
+    # Joueur 2 - verification verticale
+    elif board[0][0] == joueur_2 and board[1][0] == joueur_2 and board[2][0] == joueur_2 \
+    or board[0][1] == joueur_2 and board[1][1] == joueur_2 and board[2][1] == joueur_2 \
+    or board[0][2] == joueur_2 and board[1][2] == joueur_2 and board[2][2] == joueur_2:
+        print("HOORAY!")
+        print("Joueur 2 wins")
+        return True
+    # Joueur 2 - vérification diagonale
+    elif board[0][0] == joueur_2 and board[1][1] == joueur_2 and board[2][2] == joueur_2 \
+    or board[0][2] == joueur_2 and board[1][1] == joueur_2 and board[2][0] == joueur_2:
+        print("HOORAY!")
+        print("Joueur 1 wins")
+        return True
+    # Vérification d'une égalité
+    elif tour == 10:
+        print("C'est une égalite !")
+        print()
+        lancement_jeu()
+    else:
+        return False
+
+# Changement de joueur entre les tours
+def changement_joueur():
     global tour
-    global player
-    while tour < 6:
-        print("Tour numéro : ", tour)
-        for player in players:
-            if player == 1:
-                global symbol
-                symbol = user
-                print("Player 1, your move: ")
-            else:
-                symbol = bot
-                print("Player 2, your move: ")
-            moves()
+    global joueur_1
+    global joueur_2
+    global symbole
+    tour = 1
+    while verifie() != True:
+        if tour % 2 == 1:
+            symbole = joueur_1
+            print("Joueur 1, c'est à ton tour de jouer!")
+        else:
+            symbole = joueur_2
+            print("Joueur 2, c'est à ton tour de jouer!")
+        actions()
         tour += 1
-               
-# moves
-def moves():
-    # error if number isnt 1,2 or 3 or line is full
+                                   
+# Défini l'action des joueurs ET vérifie si les informations demandées tel que le numéro correspond aux choix proposés
+def actions():
+    # Erreur qui apparait si les éléments demandés ne sont pas respecté
     while True:
-        play1 = int(input("On which line do you wish to play ? (1,2,3) : "))
-        if play1 > 3:
+        action_1 = int(input("Sur quelle ligne voulez-vous jouer ? (1,2,3) : "))
+        if action_1 > 3 or action_1 == 0:
             print()
-            print("1- Error, Enter a number 1, 2 or 3")
+            print("1- Erreur, entrez un numéro 1, 2 ou 3")
             print()
         else:
             break
     while True:
-        play2 = int(input("Which section you wish to put your symbol ? (1,2,3) : "))
-        if play2 > 3:
+        action_2 = int(input("Dans quelle colonne ? (1,2,3) : "))
+        if action_2 > 3 or action_2 == 0:
             print()
-            print("2- Error, Enter a number 1, 2 or 3")
+            print("2- Erreur, entrez un numéro 1, 2 ou 3")
             print()
         else:
             break
-    # line 1
-    if play1 == 1:
-        if line1[play2 - 1] == "_":
-            line1[play2 - 1] = symbol
+    # Détermine une action en ligne 1 ET une erreur si l'emplacement est déjà pris
+    if action_1 == 1:
+        if ligne1[action_2 - 1] == "_":
+            ligne1[action_2 - 1] = symbole
         else:
             print()
-            print("OOPS! Looks like the spot is already taken")
+            print("OOPS! Il semblerait que cet emplacement soit déjà pris.")
+            print("Essaie-en un autre :)")
             print()   
-            return moves()           
-    # line 2
-    elif play1 == 2:
-        if line2[play2 - 1] == "_":
-            line2[play2 - 1] = symbol
+            return actions()           
+    # Détermine une action en ligne 2 ET une erreur si l'emplacement est déjà pris
+    elif action_1 == 2:
+        if ligne2[action_2 - 1] == "_":
+            ligne2[action_2 - 1] = symbole
         else:
             print()
-            print("OOPS! Looks like the spot is already taken")
+            print("OOPS! Il semblerait que cet emplacement soit déjà pris.")
+            print("Essaie-en un autre :)")
             print()
-            return moves()
-    # line 3
-    elif play1 == 3:
-        if line3[play2 - 1] == "_":
-            line3[play2 - 1] = symbol
+            return actions()
+    # Détermine une action en ligne 3 ET une erreur si l'emplacement est déjà pris
+    elif action_1 == 3:
+        if ligne3[action_2 - 1] == "_":
+            ligne3[action_2 - 1] = symbole
         else:
             print()
-            print("OOPS! Looks like the spot is already taken")
+            print("OOPS! Il semblerait que cet emplacement est pris.")
+            print("Essaie-en un autre :)")
             print()
-            return moves()
+            return actions()
     print()
-    print("|".join(line1))
-    print("|".join(line2))
-    print("|".join(line3))
+    print("|".join(ligne1))
+    print("|".join(ligne2))
+    print("|".join(ligne3))
     print()
              
-# starting the game
-def game():
+# Lancement du jeu Tik Tak Toe
+def lancement_jeu():
+    partie = input("Souhaitez-vous lancer une partie ? [O]ui/[N]on ")
     print()
-    print("Welcome to Tik Tak Toe ! ")
-    print()
-    start = input("Do you want to play ? [Y]es/[N]o ")
-    print()
-    if start.lower() != "y" and start.lower() != "yes" and start.lower() != "n" and start.lower() != "no":
-        print("Error! Enter [Y]es or [N]o ")
-        return game()
-    elif start.lower() == "n" or start.lower() == "no":
-        print("Bye !")
-    elif start.lower() == "y" or start.lower() == "yes":
-        print("|".join(line1))
-        print("|".join(line2))
-        print("|".join(line3))
+    if partie.lower() != "o" and partie.lower() != "oui" \
+    and partie.lower() != "n" and partie.lower() != "non" and partie.lower() != "no":
+        print("Erreur! Entrez [O]ui ou [N]on ")
+        return lancement_jeu()
+    elif partie.lower() == "n" or partie.lower() == "non" or partie.lower() == "no":
+        print("Aurevoir !")
         print()
-        switch_player()
+    elif partie.lower() == "o" or partie.lower() == "oui":
+        print("|".join(ligne1))
+        print("|".join(ligne2))
+        print("|".join(ligne3))
+        print()
+        changement_joueur()
 
-game()
+# Pré-lancement du jeu - Message d'Accueil
+def pre_lancement():
+    print()
+    print("Bienvenue dans Tik Tak Toe ! ")
+    print("Un monde de grilles et de symboles")
+    print()
+    lancement_jeu()
+
+pre_lancement()
+
+
+# to do list #
+# faire verif draw 
