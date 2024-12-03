712b619ea151a16db23bcaf3315836b902859a40
jams.sanchez <james.sanchez@laplateforme.io>
2024-12-03 01:51:03 +0100

    ajout du changement de joueurs + reglages des situations créant des problemes tel qu'un symbole ne peut pas etre remplacer par un autre ou un nombre qui ne correspond pas à l'input demander + petite  mise en page lors du lancement du jeu dans le terminal

--- Tik_Tak_James/tik_tak_james.py
+++ Tik_Tak_James/tik_tak_james.py
@@ -1,3 +1,6 @@
+# info
+tour = 1
+
 # players
 players = [1, 2]
 user = "X"
@@ -5,14 +8,15 @@ bot = "O"
 symbol = ""
 
 # tableau
-board = [["_", "_", "_"], ["_", "_", "_"], [" ", " ", " "]]
+board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
 line1 = board[0]
 line2 = board[1]
 line3 = board[2]
-
+            
 # switch players between rounds
 def switch_player():
     global tour
+    global player
     while tour < 6:
         print("Tour numéro : ", tour)
         for player in players:
@@ -25,36 +29,66 @@ def switch_player():
                 print("Player 2, your move: ")
             moves()
         tour += 1
-    
+               
 # moves
 def moves():
-    play1 = int(input("On which line do you wish to play ? (1,2,3) : "))
-    play2 = int(input("Which section you wish to put your symbol ? (1,2,3) : "))
-    # error if number isnt 1,2 or 3
-    if play1 != 1 and play1 != 2 and play1 != 3:
-        print("1- Error, Enter a number 1, 2 or 3")
-        return play1
-    elif play2 != 1 and play2 != 2 and play2 != 3:
-        print("2- Error, Enter a number 1, 2 or 3")
-        return play2
-    else: 
-        # line 1
-        if play1 == 1:
+    # error if number isnt 1,2 or 3 or line is full
+    while True:
+        play1 = int(input("On which line do you wish to play ? (1,2,3) : "))
+        if play1 > 3:
+            print()
+            print("1- Error, Enter a number 1, 2 or 3")
+            print()
+        else:
+            break
+    while True:
+        play2 = int(input("Which section you wish to put your symbol ? (1,2,3) : "))
+        if play2 > 3:
+            print()
+            print("2- Error, Enter a number 1, 2 or 3")
+            print()
+        else:
+            break
+    # line 1
+    if play1 == 1:
+        if line1[play2 - 1] == "_":
             line1[play2 - 1] = symbol
-        # line 2
-        elif play1 == 2:
+        else:
+            print()
+            print("OOPS! Looks like the spot is already taken")
+            print()   
+            return moves()           
+    # line 2
+    elif play1 == 2:
+        if line2[play2 - 1] == "_":
             line2[play2 - 1] = symbol
-        # line 3
-        elif play1 == 3:
+        else:
+            print()
+            print("OOPS! Looks like the spot is already taken")
+            print()
+            return moves()
+    # line 3
+    elif play1 == 3:
+        if line3[play2 - 1] == "_":
             line3[play2 - 1] = symbol
-        print("|".join(line1))
-        print("|".join(line2))
-        print("|".join(line3))
-    
+        else:
+            print()
+            print("OOPS! Looks like the spot is already taken")
+            print()
+            return moves()
+    print()
+    print("|".join(line1))
+    print("|".join(line2))
+    print("|".join(line3))
+    print()
+             
 # starting the game
 def game():
+    print()
     print("Welcome to Tik Tak Toe ! ")
+    print()
     start = input("Do you want to play ? [Y]es/[N]o ")
+    print()
     if start.lower() != "y" and start.lower() != "yes" and start.lower() != "n" and start.lower() != "no":
         print("Error! Enter [Y]es or [N]o ")
         return game()
@@ -64,6 +98,7 @@ def game():
         print("|".join(line1))
         print("|".join(line2))
         print("|".join(line3))
+        print()
         switch_player()
 
 game()
