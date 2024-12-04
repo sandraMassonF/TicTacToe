25def4f354cb9274618e35c32d065130c1437e29
Bossola-Yannick <bossolay@hotmail.fr>
2024-12-04 16:59:35 +0100

    feat: ajout fonction rejouer

--- yannick/tiktaktoe-opti.py
+++ yannick/tiktaktoe-opti.py
@@ -1,6 +1,6 @@
 # Déclaration des variables
 victoire = False
-case_prise = "False"
+case_prise = False
 selection_colonne = ""
 selection_ligne = ""
 total_selection= []
@@ -50,22 +50,6 @@ def condition_victoire(ligne1, ligne2, ligne3, joueur):
         victoire = True
         message_victoire = f"Victoire joueur {joueur} !"
     print(message_victoire)
-       
-# Vérification si case occupée
-def occuper(selection_ligne, selection_colonne, joueur):
-    global case_prise
-    case_occupe = []    
-    case_occupe.append(selection_ligne) 
-    case_occupe.append(selection_colonne)
-    if case_occupe in total_selection:
-        print("cette case à déjà été joué")
-        print(total_selection)
-        tour_joueur(joueur)
-        case_prise = "True"
-
-    else:
-        total_selection.append(case_occupe)
-        print(total_selection)
 
 # Choix du joueur/symbole
 def choix_joueur():
@@ -74,19 +58,19 @@ def choix_joueur():
     symbole = symbole.upper()
     print(symbole)
     if symbole == "O":
-        print("Le joueur 1 aura le symbole O !")
-        print("Le joueur 2 aura le symbole X !")
         joueur1 = symbole
         joueur2 = "X"
+        print("Le joueur 1 aura le symbole O !")
+        print("Le joueur 2 aura le symbole X !")
     elif symbole == "X":
-        print("Le joueur 1 aura le symbole X !")
-        print("Le joueur 2 aura le symbole O !")
         joueur1 = symbole
         joueur2 = "O"
+        print("Le joueur 1 aura le symbole X !")
+        print("Le joueur 2 aura le symbole O !")
     else:
         print("Le symbole entré n'est pas valide ! ")
         choix_joueur()
-        
+
 # on vérifie que la saisi est OK:
 def verification_saisie(selection_ligne, selection_colonne, joueur):
     if selection_ligne > 3 or selection_ligne < 1 \
@@ -95,52 +79,90 @@ def verification_saisie(selection_ligne, selection_colonne, joueur):
         print("le chiffre entré est invalide !")
         tour_joueur(joueur)
 
-# JOUEUR 1
-# demande des cohordonnés
+# Vérification si case occupée
+def occuper(selection_ligne, selection_colonne, joueur):
+    global case_prise
+    case_prise = False
+    case_occupe = []    
+    case_occupe.append(selection_ligne) 
+    case_occupe.append(selection_colonne)
+    if case_occupe in total_selection:
+        print("cette case à déjà été joué")
+        print(total_selection)
+        case_prise = True
+        tour_joueur(joueur)
+
+    else:
+        total_selection.append(case_occupe)
+        print(total_selection)
+
+# demande des chiffres
 def tour_joueur(joueur):
     global case_prise
-    case_prise = "False"
     print(f"Joueur {joueur}")
     selection_ligne = int(input("Choisissez dans quelle ligne (chiffre entre 1 et 3): "))
-    selection_colonne = int(input("Choisissez dans quelle colone (chiffre entre 1 et 3): "))
+    selection_colonne = int(input("Choisissez dans quelle colonne (chiffre entre 1 et 3): "))
     # on vérifie que la saisi est OK:
     verification_saisie(selection_ligne, selection_colonne, joueur)
     # on vérifie si la case est occupée:
     occuper(selection_ligne,selection_colonne, joueur)
     # on ecrit la selection du joueur dans la grille:
-    if selection_ligne == 1:
-        ligne1[selection_colonne-1] = joueur
-                    
-    elif selection_ligne == 2:
-        ligne2[selection_colonne-1] = joueur
-                    
-    elif selection_ligne == 3:
-        ligne3[selection_colonne-1] = joueur        
-    # mise à jour de la grille
-    grille()
-    condition_victoire(ligne1, ligne2, ligne3, joueur)
+    if case_prise != True:
+        if selection_ligne == 1:
+            ligne1[selection_colonne-1] = joueur              
+        elif selection_ligne == 2:
+            ligne2[selection_colonne-1] = joueur              
+        elif selection_ligne == 3:
+            ligne3[selection_colonne-1] = joueur        
+        # mise à jour de la grille
+        grille()
+        condition_victoire(ligne1, ligne2, ligne3, joueur)
+
+# restart partie
+def restart():
+    global total_selection, joueur, victoire, case_prise, selection_colonne,\
+    selection_ligne, message_victoire, ligne1, ligne2, ligne3
+    restart = input("refaire une partie ? oui ou non :")
+    if restart == "oui":
+        ligne1 = ["-","-","-"]
+        ligne2 = ["-","-","-"]
+        ligne3 = ["-","-","-"]
+        victoire = False
+        case_prise = False
+        selection_colonne = ""
+        selection_ligne = ""
+        total_selection= []
+        message_victoire = ""
+        joueur = ""
+        grille()
+        start_game()
+    else:
+        print("     _     ")
+        print("    | |    ")
+        print("   _| |_   ")
+        print(" _| | | |_ ")
+        print("| | | | | |")
+        print("|_|_|_|_|_|")
+        quit()
+       
 
 # selection de la ligne joué
-def turn_game():
+def start_game():
     choix_joueur()
     while victoire != True:
         tour_joueur(joueur1)
         if victoire == True:
-            break
-        while case_prise == "True":
+            restart()
+        while case_prise == True:
             tour_joueur(joueur1)
         if len(total_selection) == 9:
             print("Dommage personne à réussi à gagner !")
-            restart = input("refaire une partie ? oui ou non :")
-            if restart == "oui":
-                turn_game()
-            else:
-                ("Ciao ! ")
-                break
+            restart()
         tour_joueur(joueur2)
         if victoire == True:
-            break
-        while case_prise == "True":
+            restart()
+        while case_prise == True:
             tour_joueur(joueur2)
         
-turn_game()
+start_game()
+
