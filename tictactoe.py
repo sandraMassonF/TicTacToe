a2d9d2f5a0743579d014f51fcc47ae8cdbf7d255
sandraMasson-F <sandra.bacot@laplateforme.io>
2024-12-04 13:56:19 +0100

    finalisation du code

--- tictactoe.py
+++ tictactoe.py
@@ -12,8 +12,9 @@ def jouer():
     affichage_grille()
     while fin_jeu == False:
        tour(joueur_actuel)
-       verifier_fin_jeu():
-
+       verifier_fin_jeu()
+       joueur_suivant()
+    resultat()
        
 
 def choix_joueur():
@@ -41,27 +42,6 @@ def affichage_grille():
     print("-------------")
     print("\n")
 
-def tour(joueur):
-   print("C'est le tour du joueur ", joueur)
-   pos = input("Choisissez un espace libre sur la grille entre 1 et 9 : ")
-
-   valide = False
-   while valide == False :
-      while pos not in ["1","2","3","4","5","6","7","8","9"]:
-         pos = input("Choisissez un espace libre sur la grille entre 1 et 9 : ")
-      pos = int(pos) - 1
-
-      if grille[pos] == "-":
-         valide = True
-      else:
-         print("Cet emplacement n'est pas disponible")
-        
-grille[pos] = joueur
-affichage_grille()
-
-def verifier_fin_jeu():
-   verifier_victoire()
-   verifier_match_nul()
 
 def verifier_victoire():
     global fin_jeu
@@ -81,7 +61,7 @@ def verifier_victoire():
     elif grille[1]==grille[4]==grille[7] and grille[7] != "-":
         fin_jeu = True
         gagnant = grille[7]
-    elif grille[3]==grille[5]==grille[8] and grille[8] != "-":
+    elif grille[2]==grille[5]==grille[8] and grille[8] != "-":
         fin_jeu = True
         gagnant = grille[8]
     elif grille[0]==grille[4]==grille[8] and grille[8] != "-":
@@ -91,10 +71,48 @@ def verifier_victoire():
         fin_jeu = True
         gagnant = grille[6]
 
+
+def tour(joueur):
+   print("C'est le tour du joueur ", joueur)
+   pos = input("Choisissez un espace libre sur la grille entre 1 et 9 : ")
+
+   valide = False
+   while valide == False :
+      while pos not in ["1","2","3","4","5","6","7","8","9"]:
+         pos = input("Choisissez un espace libre sur la grille entre 1 et 9 : ")
+      pos = int(pos) - 1 
+
+      if grille[pos] == "-":
+         valide = True
+      else:
+         print("Cet emplacement n'est pas disponible")
+        
+   grille[pos] = joueur
+   affichage_grille()
+
+
+def verifier_fin_jeu():
+   verifier_victoire()
+   verifier_match_nul()
+
+
 def verifier_match_nul():
    global fin_jeu
    if "-" not in grille :
       fin_jeu = True
+
+def joueur_suivant():
+   global joueur_actuel
+   if joueur_actuel == "X":
+      joueur_actuel = "O"
+   else:
+      joueur_actuel = "X"
+
+def resultat():
+   if gagnant == "X" or gagnant == "O":
+      print("Le joueur : ", gagnant, "a gagné")
+   else:
+      print("Match nul.")
 
 jouer()
 
